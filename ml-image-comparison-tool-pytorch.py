186687d1d18c70e5542240c2140b6f26295c466e
mattlitz <matt.litz@protonmail.ch>
2023-10-25 23:38:47 -0400

    added dataset class

--- ml-image-comparison-tool-pytorch.py
+++ ml-image-comparison-tool-pytorch.py
@@ -45,21 +45,40 @@ train_csv = r'image_path.csv'
 def load_images(path):
     img = imread(path)
     img = gray2rgb(img) #convert to rgb for transfer learning
-    return np.array(np.float16(img))
+    return np.array(np.uint8(img))
  
 
 with open(train_csv,"r") as f:
     data_dict = {'image': [], 'label': []}
-    reader = csv.reader(f)
-    next(reader)
+    reader = csv.DictReader(f)
     for row in reader:
-        image_path = row[0]
+        image_path = row['path']
         data_dict['image'].append(load_images(image_path))     
-        data_dict['label'].append(np.uint8(row[1]))
+        data_dict['label'].append(np.uint8(row['label']))
 
 
 
 #%% Load torch dataset
+
+class trainDataset(torch.utils.data.Dataset):
+    """Some Information about MyDataset"""
+    def __init__(self, data_dict, transform=None):
+        super(trainDataset, self).__init__()
+        self.data_dict = data_dict
+        self.transform = transform
+
+    def __getitem__(self, index):
+
+        image = self.data_dict['image'][index]
+        label = torch.tensor(self.data_dict['label'][index])
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label 
+
+    def __len__(self):
+        return len(self.data_dict['image'])
 
 
 # Define the transforms to apply to the images
@@ -69,22 +88,15 @@ transform = transforms.Compose([
     ])
 
 
-# Define the fraction of data to use for validation
-val_fraction = 0.2
 
-# Split the data into training and validation sets
-train_size = int(len(data_dict['image']) * (1 - val_fraction))
-val_size = len(data_dict) - train_size
-train_data, val_data = data.random_split(data_dict, [train_size, val_size])
+#%% Split training data into 80/20 train/val
 
+train_dataset = trainDataset(data_dict, transform=transform)
+train_data, val_data = train_test_split(train_dataset, test_size=0.2, random_state=42)
 
 # Create the dataloaders
-train_dataset = data.TensorDataset(transform(train_data['image']), torch.tensor(train_data['label']))
-train_dataloader = data.DataLoader(train_dataset, batch_size=3, shuffle=True, num_workers=4)
-
-val_dataset = data.TensorDataset(transform(val_data['image']), torch.tensor(val_data['label']))
-val_dataloader = data.DataLoader(val_dataset, batch_size=3, shuffle=False, num_workers=4)
-
+train_dataloader = data.DataLoader(train_data, batch_size=4, shuffle=True, num_workers=4)
+val_dataloader = data.DataLoader(val_data, batch_size=4, shuffle=False, num_workers=4)
 
 
 #%% Run models
@@ -107,7 +119,8 @@ optimizer = optim.Adam(model.parameters(), lr=0.001)
 
 # Train the model
 num_epochs = 10
-for epoch in range(num_epochs):
+#for epoch in range(num_epochs):
+for batch, (images, labels) in enumerate(train_dataloader,1):
     # Train the model for one epoch
     print('Epoch {}/{}'.format(epoch+1, num_epochs))
     model.train()
@@ -125,6 +138,7 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
         running_loss += loss.item() * inputs.size(0)
+
     epoch_loss = running_loss / len(train_dataset)
 
     # Evaluate the model on the validation set
@@ -143,3 +157,4 @@ for epoch in range(num_epochs):
 
     # Print the loss and accuracy for this epoch
     print('Epoch [{}/{}], Loss: {:.4f}, Val Acc: {:.4f}'.format(epoch+1, num_epochs, epoch_loss, epoch_acc))
+# %%
