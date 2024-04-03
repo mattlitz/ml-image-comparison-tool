6d0bb7913024d63be92f2feaa5e622410d948ed1
Matt Litz <matt.litz@protonmail.ch>
2024-04-02 22:13:44 -0400

    added glcm textures, BayesGridSearchCV

--- ml-image-comparison-tool.py
+++ ml-image-comparison-tool.py
@@ -9,35 +9,28 @@ import numpy as np
 import pandas as pd
 import csv
 import generate_textures as gt
+from scipy.stats import skew, kurtosis
 
 from sklearnex import patch_sklearn
 patch_sklearn()
 
 #read images
 from skimage.io import imread, imshow
+from skimage.texture import greycomatrix, greycoprops
 import matplotlib.pyplot as plt
 
 #scikit-learn methods
-from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
-from sklearn.gaussian_process import GaussianProcessClassifier, GaussianProcessRegressor
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier, HistGradientBoostingClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.linear_model import LogisticRegression
 
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import RocCurveDisplay, auc
+
 
 #basyesian optimization
-from skopt import gp_minimize
-
-#res = gp_minimize(f,                  # the function to minimize
-#                  [(-2.0, 2.0)],      # the bounds on each dimension of x
-#                  acq_func="EI",      # the acquisition function
-#                  n_calls=15,         # the number of evaluations of f
-#                  n_random_starts=5,  # the number of random initialization points
-#                  noise=0.1**2,       # the noise level (optional)
-#                  random_state=1234)   # the random seed
-
-#pytorch neural nets
-import torch
+from skopt import BayesGridSearchCV
 
 
 import mlflow
@@ -82,6 +75,7 @@ with open(train_csv,"r") as f:
     for row in reader:
         image_path = row['path']
         label_name = row['label']
+
         data_dict['image'].append(load_images(image_path))   
         data_dict['filename'].append(image_path)   
         data_dict['label'].append(label_name) 
@@ -89,16 +83,52 @@ with open(train_csv,"r") as f:
         data_dict['otsu_segment'].append(gt.otsu_segment(load_images(image_path)))
         data_dict['kmeans_segment'].append(gt.kmeans_segment(load_images(image_path),5)) 
 
+    #calculate GLCM textures
+    glcm = greycomatrix(load_images(image_path), distances=[1], angles=[0], levels=256, symmetric=True, normed=True)
+    data_dict['Contrast'].append(greycoprops(glcm, 'contrast')[0, 0]),
+    data_dict['Dissimilarity'].append(greycoprops(glcm, 'dissimilarity')[0, 0]),
+    data_dict['Homogeneity'].append(greycoprops(glcm, 'homogeneity')[0, 0]),
+    data_dict['Energy'].append(greycoprops(glcm, 'energy')[0, 0]),
+    data_dict['Correlation'].append(greycoprops(glcm, 'correlation')[0, 0]),
+    data_dict['ASM'].append(greycoprops(glcm, 'ASM')[0, 0])
+
+#prepare feature data for training
+feature_df = pd.DataFrame(columns=['Image', 'Mean', 'Skew', 'Kurtosis', 'Variance', 'contrast', 'dissimilarity', 'homogeneity', 'energy', 'correlation', 'ASM', 'Target'])
+
+# Loop over the images in the dictionary
+for image_name, image in data_dict.items():
+    #calculate GLCM textures
+    glcm = greycomatrix(image, distances=[1], angles=[0], levels=256, symmetric=True, normed=True)
+
+    # Calculate the statistical features and store them in the DataFrame
+    feature_df = feature_df.append({
+        'Image': image_name,
+        'Mean': np.mean(image),
+        'Skew': skew(image.flatten()),
+        'Kurtosis': kurtosis(image.flatten()),
+        'Variance': np.var(image),
+        'contrast': greycoprops(glcm, 'contrast')[0, 0], 
+        'dissimilarity': greycoprops(glcm, 'dissimilarity')[0, 0], 
+        'homogeneity': greycoprops(glcm, 'homogeneity')[0, 0], 
+        'energy':greycoprops(glcm, 'energy')[0, 0], 
+        'correlation': greycoprops(glcm, 'correlation')[0, 0], 
+        'asm': greycoprops(glcm, 'ASM')[0, 0],
+        'Target': 'Target'
+    }, ignore_index=True)
 
 
 #%% Tune hyperparameters
+params = {}
+params['loss'] = ('log_loss', 'exponential')
+params['n_estimators'] = (50, 100, 150, 200)
+params['max_depth'] = (3, 5, 7, 9)
+params['learning_rate'] = (0.01, 0.1, 0.5, 1.0)
+params['min_samples_split'] = (2, 4, 6, 8)
 
 
 
 
-
-
-#%% Classify image with GPC
+#%% Classify image with classifiers
 
 images = data_dict['image']
 labels = data_dict['label']
@@ -107,49 +137,51 @@ images = [img.flatten() for img in images]
 scaler = StandardScaler()
 images = scaler.fit_transform(images)
 
-# Split the data into training and testing sets
-images_train, images_test, labels_train, labels_test = train_test_split(images, labels, test_size=0.2, random_state=42)
+tprs = []
+aucs = []
+mean_fpr = np.linspace(0, 1, 100)
 
 # Define the classifier
-gpc = GaussianProcessClassifier(random_state=0)
+gbc = GradientBoostingClassifier()
 
-# Train the classifier
-gpc.fit(images_train, labels_train)
+n_splits = 10
+cv = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42)
+
+opt = BayesGridSearchCV(gbc, params, cv=cv)
+
+for fold, (train, test) in enumerate(cv.split(images, labels)):
+    opt.fit(X[train], y[train])
+    viz = RocCurveDisplay.from_estimator(
+        opt,
+        X[test],
+        y[test],
+        name=f"ROC fold {fold}",
+        alpha=0.3,
+        lw=1,
+        ax=ax,
+        plot_chance_level=(fold == n_splits - 1),
+    )
+    interp_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
+    interp_tpr[0] = 0.0
+    tprs.append(interp_tpr)
+    aucs.append(viz.roc_auc)
+
+mean_tpr = np.mean(tprs, axis=0)
+mean_tpr[-1] = 1.0
+mean_auc = auc(mean_fpr, mean_tpr)
+std_auc = np.std(aucs)
+ax.plot(
+    mean_fpr,
+    mean_tpr,
+    color="b",
+    label=r"Mean ROC (AUC = %0.2f $\pm$ %0.2f)" % (mean_auc, std_auc),
+    lw=2,
+    alpha=0.8,
+)
+
 
 # Predict the labels of the test set
 labels_pred = gpc.predict(images_test)
-
-
-#%% Regress image with GPR
-
-# Assume 'data_dict' is your dictionary containing two image keys and labels
-# 'image1' and 'image2' are lists of images and 'label' is a list of labels
-images = data_dict['image']
-entropy = data_dict['entropy']
-labels = data_dict['label']
-
-
-# Flatten and standardize images
-images = [img.flatten() for img in images]
-entropy = [img.flatten() for img in entropy]
-scaler = StandardScaler()
-images = scaler.fit_transform(images)
-entropy = scaler.fit_transform(entropy)
-
-# Combine the two image datasets
-textures = np.hstack((images, entropy))
-
-# Split the data into training and testing sets
-images_train, images_test, labels_train, labels_test = train_test_split(textures, labels, test_size=0.2, random_state=42)
-
-# Define the regressor
-gpr = GaussianProcessRegressor(random_state=0)
-
-# Train the regressor
-gpr.fit(images_train, labels_train)
-
-# Predict the labels of the test set
-labels_pred = gpr.predict(images_test)
 
 
 # %% Calculate metrics
